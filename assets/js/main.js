c6d0b61bf5a80d2d94de8571853a1616133ec7c9
Fellix Allenfant <137398658+JackGame31@users.noreply.github.com>
2023-12-20 01:12:22 +0700

    Base File
    Base file for schedule maker

---
+++ assets/js/main.js
@@ -0,0 +1,252 @@
+// for index id
+window.onload = function () {
+  // update index id of items to the latest
+  update();
+
+  // for modal
+  const summaryModal = document.getElementById("summaryModal");
+  if (summaryModal) {
+    summaryModal.addEventListener("show.bs.modal", (event) => {
+      // Update the modal's content
+      const text = summaryModal.querySelector("#summary_text");
+      text.value = "";
+
+      // get current date
+      const today = new Date();
+      const date =
+        today.getDate() +
+        "-" +
+        (today.getMonth() + 1) +
+        "-" +
+        today.getFullYear();
+      text.value += `== Schedule ${date} ==`;
+
+      // get all items, start and end time
+      const items = document.querySelectorAll(".activity__entity");
+      items.forEach((item) => {
+        const name = item.querySelector(".activity__name").textContent;
+        const time = item.querySelector(".activity__time").textContent;
+        text.value += `\n${time} ${name}`;
+      });
+    });
+  }
+
+  // for notification floating
+  const toastTrigger = document.getElementById("toastClipboardBtn");
+  const toastLiveExample = document.getElementById("toastClipboard");
+
+  if (toastTrigger) {
+    const toastBootstrap =
+      bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+    toastTrigger.addEventListener("click", () => {
+      toastBootstrap.show();
+    });
+  }
+};
+
+// DRAG AND DROP
+const dropItems = document.getElementById("drop-items");
+new Sortable.create(dropItems, {
+  // for animation
+  animation: 350,
+  ghostClass: "sortable-ghost", // Class name for the drop placeholder
+  chosenClass: "sortable-chosen", // Class name for the chosen item
+  dragClass: "sortable-drag", // Class name for the dragging item
+
+  // for multi drag
+  selectedClass: "selected",
+  multiDrag: true,
+  fallbackTolerance: 3,
+  handle: ".handle",
+
+  // if data changed
+  onEnd: function (evt) {
+    update();
+  },
+});
+
+// OTHERS
+function addEvent() {
+  var list = document.getElementById("drop-items");
+  var item = document.createElement("div");
+  item.className = "activity__entity row g-0 my-2";
+  item.innerHTML = `
+  <div class="activity__time col-3 p-2">
+    05:00 - 06:00
+  </div>
+
+  <div class="activity__card col-9 shadow border rounded-4">
+    <div class="handle row g-0 px-3 py-2">
+      <div class="col-8 d-flex flex-column d-flex justify-content-center">
+        <span class="activity__name">New Task</span>
+        <span class="activity__duration text-secondary">60 minutes</span>
+      </div>
+
+      <div class="col-4 d-flex align-items-center justify-content-end">
+        <button class="btn btn-warning mx-1 edit" data-bs-toggle="collapse" data-bs-target="#collapse-0" onclick="openEdit()"><i class='bx bx-pencil'></i></a>
+        <button class="btn btn-danger mx-1 delete"><i class='bx bx-trash'></i></a>
+      </div>
+    </div>
+    <div class="collapse px-3 pt-2 pb-3 border-top" id="collapse-0">
+      <!-- name -->
+      <div>
+        <label class="col-form-label pt-0">Activity Name</label>
+        <input type="text" class="form-control edit-name rounded-5" value="New Task" onchange="editEvent(0)">
+      </div>
+
+      <!-- duration -->
+      <div>
+        <label class="col-form-label">Duration</label>
+        <input type="time" class="form-control edit-duration rounded-5" value="01:00" onchange="editEvent(0)"/>
+      </div>
+    </div>
+    `;
+  list.appendChild(item);
+  update();
+
+  // get latest event and click show bs modal
+  const editBtn = item.querySelector(".edit");
+  console.log(editBtn);
+  editBtn.click();
+}
+
+// update id and time
+function update() {
+  var items = document.querySelectorAll(".activity__entity");
+  var current = document.getElementById("start_duration").value;
+  current = convertToMinuteFormat(current);
+  var index = 0;
+  items.forEach((item) => {
+    // set the attribute
+    item.setAttribute("data-index", index);
+    var ended =
+      parseInt(current) +
+      parseInt(item.getElementsByClassName("activity__duration")[0].innerHTML);
+    item.querySelector(".activity__time").textContent =
+      convertToTimeFormat(current) + " - " + convertToTimeFormat(ended);
+    current = ended;
+
+    // edit and delete button
+    var deleteBtn = item.querySelector(".delete");
+    var editBtn = item.querySelector(".edit");
+    deleteBtn.setAttribute("onclick", "deleteActivity(" + index + ")");
+    editBtn.setAttribute("data-bs-target", "#collapse-" + index);
+
+    // change all attributes that connected with id
+    var collapse = item.querySelector(".collapse");
+    collapse.setAttribute("id", "collapse-" + index);
+    
+    // input update
+    var name = item.querySelector(".edit-name");
+    var duration = item.querySelector(".edit-duration");
+    name.setAttribute("onchange", "editEvent(" + index + ")");
+    duration.setAttribute("onchange", "editEvent(" + index + ")");
+
+    // update
+    if (document.getElementsByClassName("activity__empty").length > 0) {
+      document.getElementsByClassName("activity__empty")[0].remove();
+    }
+    index++;
+  });
+}
+
+// delete an activity
+function deleteActivity(id) {
+  var item = document.querySelector(`[data-index="${id}"]`);
+  item.remove();
+  update();
+
+  if (document.getElementById("drop-items").childElementCount == 0) {
+    document.getElementById("drop-items").innerHTML =
+      '<div class="activity__empty text-center text-secondary px-3 py-2 border border-secondary rounded-4" style="border-style: dashed !important;">No schedule created</div>';
+  }
+}
+
+// add attribute class edit mode
+// when there is class edit mode, then css selected not show
+var isEdit = -1;
+function openEdit() {
+  // check if collapse show does exist
+  var collapse = document.querySelectorAll(".show");
+  console.log(collapse);
+  if (collapse.length == 0) {
+    isEdit *= -1;
+  }
+
+  var activities = document.querySelectorAll(".activity__entity");
+  if (isEdit == 1) {
+    activities.forEach((item) => {
+      item.classList.add("edit-mode");
+    });
+  } else {
+    activities.forEach((item) => {
+      item.classList.remove("edit-mode");
+    });
+  }
+
+  // remove all selected class
+  var selected = document.querySelectorAll(".selected");
+  selected.forEach((item) => {
+    item.classList.remove("selected");
+  });
+}
+
+// edit an activity
+function editEvent(id) {
+  // get the value
+  const card = document
+    .querySelector(`[data-index="${id}"]`)
+    .querySelector(".activity__card");
+  const name = card.querySelector(".edit-name").value;
+  const duration = card.querySelector(".edit-duration").value;
+  console.log(name, duration);
+
+  // update the value
+  const item = document.querySelector(`[data-index="${id}"]`);
+  item.querySelector(".activity__name").textContent = name;
+  item.querySelector(".activity__duration").textContent =
+    convertToMinuteFormat(duration) + " minutes";
+
+  update();
+}
+
+// convert to time format (from minutes)
+function convertToTimeFormat(inputTime) {
+  // Extract the numeric value from the input string
+  const numericValue = parseInt(inputTime, 10);
+
+  // find minutes and hours
+  const minutes = numericValue % 60;
+  const hours = (numericValue - minutes) / 60;
+
+  // format string with format hh:mm
+  const timeOnly = `${hours.toString().padStart(2, "0")}:${minutes
+    .toString()
+    .padStart(2, "0")}`;
+
+  return timeOnly;
+}
+
+// convert to minute format (from time)
+function convertToMinuteFormat(inputTime) {
+  // Extract the numeric value from the input string
+  if (inputTime == "") {
+    return 0;
+  }
+  const time = inputTime.split(":");
+  const hours = parseInt(time[0], 10);
+  const minutes = parseInt(time[1], 10);
+
+  // convert to minutes
+  const minutesOnly = hours * 60 + minutes;
+
+  return minutesOnly;
+}
+
+// for copy clipboard
+function copyClipboard() {
+  const text = document.getElementById("summary_text");
+  text.select();
+  text.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+}
